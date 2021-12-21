9c5ac2930394edd6e7dae9eda83f5129f4224efa
Teodor Birghisan <birghisanteodor@yahoo.com>
2021-12-21 12:46:47 +0200

    Display different color if current text does not match target

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+
+def display_text(stdscr, target_text, current_text, wpm=0):
+    stdscr.addstr(target_text)
+
+    for pos, char in enumerate(current_text):
+        correct_char = target_text[pos]
+        color = curses.color_pair(1)
+
+        if char != correct_char:
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0, pos, char, color)
+
 wrapper(main)
