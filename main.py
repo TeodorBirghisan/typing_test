6f5789c43a0c89b5e51871c6085d692f6c2398a5
Teodor Birghisan <birghisanteodor@yahoo.com>
2021-12-21 12:45:33 +0200

    Implemented start screen

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import curses
 from curses import wrapper
 
-def main(stdscr):
+
+def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello World")
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
     stdscr.getkey()
-    
+
 wrapper(main)
